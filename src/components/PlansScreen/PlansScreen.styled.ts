2d07557194f8325774435d62bf3cd8fc0d74cbff
Zofia Perzynska <zosiaperzynskaa@wp.pl>
2022-11-29 23:43:58 +0100

    PlanScreen component added. ABCDEFG I have to go. G is for goodbye.

--- src/components/PlansScreen/PlansScreen.styled.ts
+++ src/components/PlansScreen/PlansScreen.styled.ts
@@ -1,3 +1,26 @@
 import styled from "styled-components";
 
 export const StyledProfileScreen = styled.div``;
+
+export const StyledPlan = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  opacity: 0.8;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  button {
+    padding: 10px 20px;
+    font-size: 1rem;
+    color: #fff;
+    background-color: #e50914;
+    font-weight: 600;
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+export const StyledScreenInfo = styled.div``;
--- src/components/PlansScreen/PlansScreen.tsx
+++ src/components/PlansScreen/PlansScreen.tsx
@@ -1,8 +1,65 @@
-import React from "react";
-import { StyledProfileScreen } from "./PlansScreen.styled";
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "../../firebase";
+import {
+  StyledProfileScreen,
+  StyledPlan,
+  StyledScreenInfo,
+} from "./PlansScreen.styled";
+
+interface Product {
+  active: boolean;
+  description: string;
+  images: string[];
+  metadata: any;
+  name: string;
+  role: string;
+  tax_code: string;
+}
 
 const PlansScreen = () => {
-  return <StyledProfileScreen>PlansScreen</StyledProfileScreen>;
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const productsCollectionReference = collection(db, "products");
+      const productsSnapshot = await getDocs(productsCollectionReference);
+
+      const products: Product[] = [];
+      productsSnapshot.forEach((product) => {
+        const productData = product.data() as Product;
+        if (productData.active) {
+          products.push(productData);
+        }
+      });
+      setProducts(products);
+    };
+
+    fetchData();
+    // .where("active", "==", true).get().then(QuerySnapshot => {
+    //     const products = {};
+    //     QuerySnapshot.forEach(async productDoc => {
+    //     products[productDoc.id] = productDoc.data();
+    //     })
+  }, []);
+
+  console.log(products);
+
+  return (
+    <StyledProfileScreen>
+      {products.map((product) => {
+        return (
+          <StyledPlan key={product.name}>
+            <StyledScreenInfo>
+              <h5>{product.name}</h5>
+              <h6>{product.description}</h6>
+            </StyledScreenInfo>
+            <button>Subscribe</button>
+          </StyledPlan>
+        );
+      })}
+    </StyledProfileScreen>
+  );
 };
 
 export default PlansScreen;
