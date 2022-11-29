e893c20a061fce12ccfe719debcbff2aa96b25ab
Zofia Perzynska <zosiaperzynskaa@wp.pl>
2022-11-29 22:09:32 +0100

    PlanScreen component added

---
+++ src/components/PlansScreen/PlansScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledProfileScreen } from "./PlansScreen.styled";
+
+const PlansScreen = () => {
+  return <StyledProfileScreen>PlansScreen</StyledProfileScreen>;
+};
+
+export default PlansScreen;
